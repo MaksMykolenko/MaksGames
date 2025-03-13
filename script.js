993905791fbdbc5d5a11ced19bcb223080feb2ff
Maks Mykolenko <72696352+MaksMykolenko@users.noreply.github.com>
2025-03-13 12:54:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//navbar menu for mobile version
 document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("nav-menu").classList.toggle("active");
+});
+
+//button flip-back
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".team-card").forEach((card) => {
+        const moreBtn = card.querySelector(".card-front .button");
+        const backBtn = card.querySelector(".card-back .button");
+
+        if (moreBtn && backBtn) {
+            moreBtn.addEventListener("click", () => {
+                console.log("Картка перевертається вперед");
+                card.classList.add("flipped");
+            });
+
+            backBtn.addEventListener("click", () => {
+                console.log("Картка перевертається назад");
+                card.classList.remove("flipped");
+            });
+        } else {
+            console.error("Кнопки не знайдено в картці:", card);
+        }
+    });
 });
